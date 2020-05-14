54e2e000fff004f1e24a4c4eb913bba9119d06b1
Andrew <androidandrew2@gmail.com>
2020-05-13 17:13:40 -0700

    Add bottom nav tabs, copied from a new project wizard.

---
+++ app/src/main/java/com/example/android/covid19tracker/screen_map/MapFragment.kt
@@ -0,0 +1,5 @@
+package com.example.android.covid19tracker.screen_map
+
+import androidx.fragment.app.Fragment
+
+class MapFragment : Fragment()
---
+++ app/src/main/java/com/example/android/covid19tracker/screen_region/RegionFragment.kt
@@ -0,0 +1,5 @@
+package com.example.android.covid19tracker.screen_region
+
+import androidx.fragment.app.Fragment
+
+class RegionFragment : Fragment()
