cb45d77fc6c4b3f286bab6c059f84c986cd94bb3
Andrew <androidandrew2@gmail.com>
2020-05-13 21:51:58 -0700

    Hook up bottom tab menu buttons so they navigate to new fragments Add placeholder fragment files for Region and Map.

--- app/src/main/java/com/example/android/covid19tracker/MainActivity.kt
+++ app/src/main/java/com/example/android/covid19tracker/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.android.covid19tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.generalInfoFragment, R.id.regionFragment, R.id.mapFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/android/covid19tracker/screen_region/RegionFragment.kt
+++ app/src/main/java/com/example/android/covid19tracker/screen_region/RegionFragment.kt
@@ -1,5 +1,29 @@
 package com.example.android.covid19tracker.screen_region
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.covid19tracker.databinding.FragmentRegionBinding
 
-class RegionFragment : Fragment()
+class RegionFragment : Fragment() {
+
+    private val viewModel: RegionViewModel by lazy {
+        ViewModelProviders.of(this).get(RegionViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?) : View? {
+
+        val binding = FragmentRegionBinding.inflate(inflater)
+
+        binding.setLifecycleOwner(this)
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
