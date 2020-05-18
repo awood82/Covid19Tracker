8e2a7dd4e8b514270c9a01fb80d0f85cedc037a8
Andrew <androidandrew2@gmail.com>
2020-05-18 09:43:02 -0700

    Setup a Google Map fragment  - Add permissions.  - Add test API key.  - Add some hard-coded markers

--- app/src/main/java/com/example/android/covid19tracker/screen_map/MapFragment.kt
+++ app/src/main/java/com/example/android/covid19tracker/screen_map/MapFragment.kt
@@ -1,5 +1,84 @@
 package com.example.android.covid19tracker.screen_map
 
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import com.example.android.covid19tracker.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment()
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val sfm = requireActivity().supportFragmentManager
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val geocoder = Geocoder(context)
+        val locations = mutableListOf<Address>()
+        val italy = geocoder.getFromLocationName("Italy", 1)
+        locations.add(italy[0])
+        locations.add(geocoder.getFromLocationName("USA", 1).get(0))
+        locations.add(geocoder.getFromLocationName("Spain", 1).get(0))
+        locations.add(geocoder.getFromLocationName("Italy", 1).get(0))
+        for (location in locations) {
+            googleMap?.addMarker(MarkerOptions().position(LatLng(location.latitude, location.longitude)))
+        }
+        googleMap?.addMarker(MarkerOptions().position(LatLng(-34.0, 151.0)))
+    }
+}
+
+
+/*
+OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+ */
